dc105059138828e4c2797618d716317a0d7f8423
Milan <milan.hommet@protonmail.com>
2021-11-16 10:28:54 +0100

    .

--- wfrm_prj_methodeScrum/wfrm_prj_methodeScrum/Consultation.Designer.cs
+++ wfrm_prj_methodeScrum/wfrm_prj_methodeScrum/Consultation.Designer.cs
@@ -29,12 +29,58 @@ namespace wfrm_prj_methodeScrum
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Source Code Pro", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(56, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Jeux-Vidéos";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Source Code Pro", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(56, 219);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 52);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Source Code Pro", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(56, 139);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 52);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Consoles";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Consultation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Consultation";
             this.Text = "Consultation";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
